aa92fd23145feb6cd2e3b11b46afd22d4b60977d
Kevin de Jong <59196404+dejongkevin@users.noreply.github.com>
2020-10-02 15:45:17 +0200

    Update apps.js

--- static/apps.js
+++ static/apps.js
@@ -1,7 +1,65 @@
-console.log("Kebba")
+{
+  //Get the button:
+  topButton = document.getElementById("myBtn");
 
-const knop = document.getElementById("meerovermijknop")
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
 
-knop.addEventListener("click", function() {
-  console.log("Hello World");
-});
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      topButton.style.display = "block";
+    } else {
+      topButton.style.display = "none";
+    }
+  }
+
+// When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
+}
+
+{
+  $(function() {
+    $('#overmijKnop').click(function(){
+      $('html, body').animate({
+          scrollTop: $("#aboutme").offset().top
+      }, 1200);
+  });
+    
+   })
+
+   $(function() {
+    $('#werkervaringKnop').click(function(){
+      $('html, body').animate({
+          scrollTop: $("#werkervaring").offset().top
+      }, 1000);
+  });
+    
+   })
+   $(function() {
+    $('#vaardighedenKnop').click(function(){
+      $('html, body').animate({
+          scrollTop: $("#vaardigheden").offset().top
+      }, 800);
+  });
+    
+   })
+   $(function() {
+    $('#meerovermijknop').click(function(){
+      $('html, body').animate({
+          scrollTop: $("#werkervaring").offset().top
+      }, 1200);
+  });
+    
+   })
+}
+  
+
+
+{
+  $(window).load(function() {
+    $('#loading').hide();
+  });
+}
